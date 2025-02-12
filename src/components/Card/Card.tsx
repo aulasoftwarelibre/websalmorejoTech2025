7837910d061015bbad4a8d128ec9a2c80743c6ed
darkghost078 <daviddarkghost@gmail.com>
2025-02-12 12:33:36 +0100

    fix:The animation goes well but it is not done yet

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,20 @@
-import styles from './Card.module.css'
+import styles from './Card.module.css';
 
-export default function Card({children, description, photo}) {
-
-
-
-return(
-    <div className={`${styles.card}`}>
-        <div className={`${styles.photoContainer}`}>
-            <img src={photo} alt="Foto de prueba"></img>
+export default function Card({ children, description, photo }) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.front}>
+                <div className={styles.photoContainer}>
+                    <img src={photo} alt="Foto de prueba" />
+                </div>
+                <div className={styles.text}>
+                    <h5 className={styles.NameText}>{children}</h5>
+                    <p className={styles.DescriptionText}>{description}</p>
+                </div>
+            </div>
+            <div className={styles.back}>
+                {/* Contenido de la parte posterior (opcional) */}
+            </div>
         </div>
-        <div className={`${styles.text}`}>
-            <h5 className={styles.NameText}>{children}</h5>
-            <p className={styles.DescriptionText}>{description}</p>
-        </div>
-    </div>
-);
+    );
 }
-
-
-
