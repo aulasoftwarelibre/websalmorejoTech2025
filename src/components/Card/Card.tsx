d242f16e392d1f0019265ca5bd4fa8587785b30c
darkghost078 <daviddarkghost@gmail.com>
2025-02-12 18:01:07 +0100

    fix: Added the social media buttons to the cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,43 @@
-import styles from './Card.module.css'
+import styles from './Card.module.css';
+import twLogo from '../../sprites/logosRedSocial/twLogo.svg';
+import ghLogo from '../../sprites/logosRedSocial/ghLogo.svg';
+import lkLogo from '../../sprites/logosRedSocial/lkLogo.svg';
 
-export default function Card({ children, description, photo, mirrorphoto }) {
-  return (
-    <div
-      className={styles.card}
-      style={{ backgroundImage: `url(${mirrorphoto})` }}
-    >
-      <div className={styles.front}>
-        <div className={styles.photoContainer}>
-          <img src={photo} alt="Foto de prueba" />
+export default function Card({ children, description, photo, mirrorphoto, twlink = "unknown", lklink = "unknown", ghlink = "unknown" }) {
+
+    const MostrarBoton = (boton: string) => boton !== "unknown";
+
+    return (
+        <div className={styles.card} style={{ backgroundImage: `url(${mirrorphoto})`}} >
+            <div className={styles.front}>
+                <div className={styles.photoContainer}>
+                    <img src={photo} alt="Foto de perfil" />
+                </div>
+                <div className={styles.text}>
+                    <h5 className={styles.NameText}>{children}</h5>
+                    <p className={styles.DescriptionText}>{description}</p>
+                </div>
+            </div>
+            <div className={styles.back}>
+                <div className='row'>
+
+                {MostrarBoton(ghlink) && (
+                    <a href={ghlink} target="_blank" rel="noopener noreferrer">
+                        <img src={ghLogo} alt="GitHub" className={styles.icon} />
+                    </a>
+                )}
+                {MostrarBoton(lklink) && (
+                    <a href={lklink} target="_blank" rel="noopener noreferrer">
+                        <img src={lkLogo} alt="LinkedIn" className={styles.icon} />
+                    </a>
+                )}
+                {MostrarBoton(twlink) && (
+                    <a href={twlink} target="_blank" rel="noopener noreferrer">
+                        <img src={twLogo} alt="Twitter" className={styles.icon} />
+                    </a>
+                )}
+                </div>
+            </div>
         </div>
-        <div className={styles.text}>
-          <h5 className={styles.NameText}>{children}</h5>
-          <p className={styles.DescriptionText}>{description}</p>
-        </div>
-      </div>
-      <div className={styles.back}></div>
-    </div>
-  )
+    );
 }
--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,7 +1,7 @@
 import styles from './Cards.module.css'
 import Card from '../Card/Card'
 import fotoPrueba from '../../sprites/PeoplePhotos/fotoPrueba.jpeg'
-import mirrorPrueba from '../../sprites/PeoplePhotos/mirrorprueba.jpg'
+import mirrorPrueba from '../../sprites/PeoplePhotos/mirrorphoto.jpg'
 
 export default function Cards() {
     return (
@@ -9,14 +9,14 @@ export default function Cards() {
         <div className="container">
             <div className="row justify-content-center">
             <div className="col-md-4">
-                <Card description="prueba 1" photo={fotoPrueba} mirrorphoto={mirrorPrueba}> Nombre 1</Card>
+                <Card description="prueba 1" photo={fotoPrueba} twlink= "https://www.google.es/" lklink= "https://www.google.es/" ghlink= "https://www.google.es/" mirrorphoto={mirrorPrueba}> Nombre 1</Card>
             </div>
-            <div className="col-md-4">
+            {/*<div className="col-md-4">
                 <Card description="prueba 2">Nombre 2</Card>
             </div>
             <div className="col-md-4">
                 <Card description="prueba 3">Nombre 3</Card>
-            </div>
+            </div>*/}
             </div>
         </div>
         </>
