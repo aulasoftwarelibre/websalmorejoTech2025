5ae3cc40ad0b302fd232796fae9e3c3c4b928b8e
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-02-12 17:29:40 +0100

    fix: footer "responsive"

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -48,7 +48,7 @@ export default function Info() {
           </div>
         </div>
       </div>
-      <div className="row gx-lg-5 gx-3 align-items-center container-fluid mt-4">
+      <div className={`${styles.carouselPart} row gx-lg-5 gx-3 align-items-center container-fluid`}>
         <div className="col-12 col-md-6 mb-4 mb-md-0">
           <div className={`${styles.backgroundTextoCarrousel}`}>
             <div className={`${styles.textoCarrousel}`}>
@@ -62,7 +62,7 @@ export default function Info() {
           <Swiper
             modules={[Pagination, EffectCoverflow, Autoplay]}
             pagination={{ clickable: true }}
-            effect="coverflow"
+            effect="coverflow"  
             grabCursor={true}
             centeredSlides={true}
             spaceBetween={0}
