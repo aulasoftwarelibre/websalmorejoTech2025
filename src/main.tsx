ea47515c794598050865da9ad580c0834536ed6b
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-02-28 18:14:45 +0100

    fix: now can build

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Past from './pages/Past/Index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
