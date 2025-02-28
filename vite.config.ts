29b3ac2a79805107960fe8a3d960626bc4c4a635
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-02-28 17:28:22 +0100

    fix: routes trouble

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2025/',
+  base: '/2025/', // Ensures Vite generates correct URLs for assets
+  build: {
+    outDir: 'dist',
+  }
 })
