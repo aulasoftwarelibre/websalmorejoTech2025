c96b8080d22afee8c2ded69d7ef337b69c59385a
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-02-28 18:19:01 +0100

    fix: routes not declared in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter as BrowserRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Past from './pages/Past/Index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/2025/" element={<App />} />
+        <Route path="/2025/past" element={<Past />} />
+      </Routes>
+    </Router>
   </StrictMode>
 )
