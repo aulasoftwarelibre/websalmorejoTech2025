ce5a764d30471689217b099998392f4fbd70dabf
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-03-27 11:32:45 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,22 @@ import Info from './components/InfoTest/Info';
 import { SponsorProps } from './components/Sponsors';
 import sponsors from './data/sponsors.json';
 import { SponsorsGrid } from './components/Sponsors/grid';
-import Loader from './components/Loader';
+//import Loader from './components/Loader';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [animationClass, setAnimationClass] = useState(''); // Add class to control page animation
+  // const [loading, setLoading] = useState(true);
+  // const [animationClass, setAnimationClass] = useState(''); // Add class to control page animation
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-    //  setTimeout(() => {
-        setLoading(false); // Hide loader after the slide-out animation is complete
-    //  }, 1500); // This should match the duration of the slide-out animation
-     // setAnimationClass('slide-out'); // Trigger slide-out animation for the entire page
-    }, 1800); // Time before starting the slide-out animation
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //   //  setTimeout(() => {
+  //       setLoading(false); // Hide loader after the slide-out animation is complete
+  //   //  }, 1500); // This should match the duration of the slide-out animation
+  //    // setAnimationClass('slide-out'); // Trigger slide-out animation for the entire page
+  //   }, 1800); // Time before starting the slide-out animation
 
-    return () => {clearTimeout(timer)}; 
-  }, []);
+  //   return () => {clearTimeout(timer)}; 
+  // }, []);
 
   // if (loading) {
   //   return (
