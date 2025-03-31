c2fd89d5b568539ffd1c28f1b999c4d58e658e51
Arturo Vicente Pérez <55533930+ARVIPE@users.noreply.github.com>
2025-03-31 19:41:07 +0200

    Revert "Update App.tsx"
    This reverts commit fd57f9b2442e60903460e9cae9c621856a99e587.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/HeaderPhone/Header';
@@ -7,30 +7,30 @@ import Info from './components/InfoTest/Info';
 import { SponsorProps } from './components/Sponsors';
 import sponsors from './data/sponsors.json';
 import { SponsorsGrid } from './components/Sponsors/grid';
-import Loader from './components/Loader';
+//import Loader from './components/Loader';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [animationClass, setAnimationClass] = useState(''); // Add class to control page animation
+  // const [loading, setLoading] = useState(true);
+  // const [animationClass, setAnimationClass] = useState(''); // Add class to control page animation
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-    //  setTimeout(() => {
-        setLoading(false); // Hide loader after the slide-out animation is complete
-    //  }, 1500); // This should match the duration of the slide-out animation
-     // setAnimationClass('slide-out'); // Trigger slide-out animation for the entire page
-    }, 1800); // Time before starting the slide-out animation
+  // useEffect(() => {
+  //   const timer = setTimeout(() => {
+  //   //  setTimeout(() => {
+  //       setLoading(false); // Hide loader after the slide-out animation is complete
+  //   //  }, 1500); // This should match the duration of the slide-out animation
+  //    // setAnimationClass('slide-out'); // Trigger slide-out animation for the entire page
+  //   }, 1800); // Time before starting the slide-out animation
 
-    return () => {clearTimeout(timer)}; 
-  }, []);
+  //   return () => {clearTimeout(timer)}; 
+  // }, []);
 
-  if (loading) {
-    return (
-      <div className={`appContainerLogo loader-container ${animationClass}`}>
-        <Loader />
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className={`appContainerLogo loader-container ${animationClass}`}>
+  //       <Loader />
+  //     </div>
+  //   );
+  // }
 
   return (
     <>
