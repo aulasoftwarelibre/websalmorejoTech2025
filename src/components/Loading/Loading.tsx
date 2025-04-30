4e92b175808883c50721097d7fa4388313d7eb1a
Darío Martínez Kostyuk <dariomartk@gmail.com>
2025-04-30 16:37:20 +0200

    Fix: minimal loading screen of 2 seconds

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Loading.css';
+import React from 'react'
+import './Loading.css'
 import tomate from '../../sprites/tomatito1.svg'
 
 const Loading: React.FC = () => {
@@ -7,7 +7,7 @@ const Loading: React.FC = () => {
     <div className="loading-container">
       <img src={tomate} alt="Loading..." className="loading-sprite" />
     </div>
-  );
-};
+  )
+}
 
-export default Loading;
+export default Loading
--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,46 @@
 import React from 'react'
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Loading from './components/Loading/Loading.tsx'; // Correctly import the Loading component
-//import App from './App.tsx';
-//import Past from './pages/Past/Index.tsx';
-const LazyApp =React.lazy(()=> import('./App.tsx'));
-const LazyPast =React.lazy(()=> import('./pages/Past/Index.tsx'));
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Loading from './components/Loading/Loading.tsx'
 
+const lazyLoadWithDelay = (
+  importer: () => Promise<{ default: React.ComponentType }>
+) => {
+  return Promise.all([
+    importer(),
+    new Promise((resolve) => setTimeout(resolve, 2000)),
+  ]).then(([module]) => module)
+}
+
+const LazyApp = React.lazy(() => lazyLoadWithDelay(() => import('./App.tsx')))
+const LazyPast = React.lazy(() =>
+  lazyLoadWithDelay(() => import('./pages/Past/Index.tsx'))
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Router>
-        <Routes>
-          <Route path="/2025/" element={
+      <Routes>
+        <Route
+          path="/2025/"
+          element={
             <React.Suspense fallback={<Loading />}>
               <LazyApp />
             </React.Suspense>
-            }/>
-          <Route path="/2025/past" element={
+          }
+        />
+        <Route
+          path="/2025/past"
+          element={
             <React.Suspense fallback={<Loading />}>
               <LazyPast />
             </React.Suspense>
-          }/>
-        </Routes>
+          }
+        />
+      </Routes>
     </Router>
   </StrictMode>
-);
+)
